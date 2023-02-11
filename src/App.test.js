a2e94cff6f8c67021bd68eae5e5222b3c46eb742
Isabella <isas21@student.bth.se>
2023-02-11 18:43:19 +0000

    Kmom03 test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Components
+import Header from "./components/Header";
+import NewDoc from "./components/NewDoc";
+
+// React
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+test('Checks if the header Text Editor is there', () => {
+  render(<App />, { wrapper: MemoryRouter });
+  const headerElement = screen.getByText(/Text Editor/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('Checks if the New document button is on the page', () => {
+  render(<Header />, { wrapper: MemoryRouter });
+  const new_button = screen.getByText(/New document/i);
+  expect(new_button).toBeTruthy();
+});
+
+test('Checks if the Edit document button is on the page', () => {
+  render(<Header />, { wrapper: MemoryRouter });
+  const edit_button = screen.getByText(/Edit document/i);
+  expect(edit_button).toBeTruthy();
+});
+
+test('Checks if the Save button is on the page', () => {
+  render(<NewDoc />, { wrapper: MemoryRouter });
+  const save_button = screen.getByText(/Save/i);
+  expect(save_button).toBeTruthy();
 });
